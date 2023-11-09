060f879a7c0e82f7263f12018785410185c97b74
LBF38 <contact.lbf38@gmail.com>
2023-11-09 11:57:21 +0100

    test(api): :white_check_mark: update sysinfo tests w/ mock data
    using Jest mocking system, we mock the deps
    and choose how the mock data is shaped
    according to our needs for the test of the function we defined.

--- src/system_information.spec.ts
+++ src/system_information.spec.ts
@@ -1,28 +1,42 @@
-import { getAllData } from "systeminformation";
+import { Systeminformation, getAllData } from "systeminformation";
 import { getSystemInformation } from "./system_information";
 
-jest.mock("systeminformation", () => ({
-  getAllData: jest.fn(async () => {
-    return jest.fn();
-  })
-}));
+jest.mock("systeminformation");
+
+const mockedGetAllData = getAllData as jest.MockedFunction<typeof getAllData>;
+
 
 describe('Testing getSystemInformation', () => {
-  it('should return the sysinfo from our interface', async () => {
+  beforeEach(() => {
+    mockedGetAllData.mockClear();
+  });
 
+  it('should return the sysinfo from our interface', async () => {
+    // Arrange
+    const mockData: Awaited<ReturnType<typeof getAllData>> = {
+      cpu: {},
+      system: {},
+      mem: {},
+      os: {},
+      currentLoad: {},
+      processes: {},
+      diskLayout: {},
+      net: {}
+    } as Awaited<ReturnType<typeof getAllData>>;
+    mockedGetAllData.mockResolvedValue(mockData);
     // Act
     const sysinfo = await getSystemInformation();
-    console.log("TODO: implement a correct test");
+    // console.log("TODO: implement a correct test");
     // Assert
-    expect(getAllData).toHaveBeenCalled();
+    expect(mockedGetAllData).toHaveBeenCalled();
     // console.log("Test Passed");
-    // expect(sysinfo.cpu).toBeDefined();
-    // expect(sysinfo.currentLoad).toBeDefined();
-    // expect(sysinfo.diskLayout).toBeDefined();
-    // expect(sysinfo.mem).toBeDefined();
-    // expect(sysinfo.networkInterfaces).toBeDefined();
-    // expect(sysinfo.os).toBeDefined();
-    // expect(sysinfo.processes).toBeDefined();
-    // expect(sysinfo.system).toBeDefined();
+    expect(sysinfo.cpu).toBeDefined();
+    expect(sysinfo.currentLoad).toBeDefined();
+    expect(sysinfo.diskLayout).toBeDefined();
+    expect(sysinfo.mem).toBeDefined();
+    expect(sysinfo.networkInterfaces).toBeDefined();
+    expect(sysinfo.os).toBeDefined();
+    expect(sysinfo.processes).toBeDefined();
+    expect(sysinfo.system).toBeDefined();
   });
 });
